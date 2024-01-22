7a7ceb88925bfc5ef351a2223b80297a63df4dbd
agnesvarga1 <agikolyok@gmail.com>
2024-01-22 13:08:38 +0100

    arr objs created, initial html structure

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-alert("hello");
+const bPArr = [
+  {
+    img: "budapest-10.jpg",
+    title: "Parlament and Danube",
+    descript:
+      "Budapest, Hungary's capital on the Danube, mesmerizes with iconic landmarks like Parliament and Buda Castle. The Jewish Quarter's ruin bars and diverse cuisine add vibrancy to this enchanting city.",
+  },
+  {
+    img: "budapest-50.jpg",
+    title: "Fisherman's Bastion",
+    descript:
+      "At Buda's Castle Hill, the Hungarian Fisherman's Bastion is a fairytale-like terrace offering panoramic views of Budapest. This neo-Gothic and neo-Romanesque architectural gem, adorned with seven towers, enchants visitors with its whimsical charm.",
+  },
+  {
+    img: "budapest-72.jpg",
+    title: "Budapest Castle Hill",
+    descript:
+      " Matthias Church in Budapest, a Gothic masterpiece, captivates with its stunning architecture and historical significance on Buda's Castle Hill.",
+  },
+  {
+    img: "budapest-76.jpg",
+    title: "St. Stephen's Basilica",
+    descript:
+      "St. Stephen's Basilica in Budapest stands as a neoclassical marvel, housing the revered Saint Stephen's mummified hand and offering breathtaking city panoramas. He had six fingers on his right hand",
+  },
+  {
+    img: "budapest-104.jpg",
+    title: "Széchenyi Chain Bridge",
+    descript:
+      "Széchenyi Chain Bridge, an iconic Budapest link across the Danube, combines stunning aesthetics with historical significance, connecting Buda and Pest.",
+  },
+  {
+    img: "budapest-122.jpg",
+    title: "Hungarian Parliament",
+    descript:
+      "Budapest's Parliament, an architectural marvel, boasts intricate details inspired by Gothic and Renaissance styles, overlooking the Danube as a symbol of Hungary's grandeur.",
+  },
+];
